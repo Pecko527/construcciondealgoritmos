df7b8e39e458cf1c85912b9bb2b60f4686c9f4ae
Pecko527 <160885084+Pecko527@users.noreply.github.com>
2024-02-28 12:40:16 -0500

    Delete cuentaAhorros.py

--- cuentaAhorros.py
+++
@@ -1,7 +0,0 @@
-class cuenta_ahorros:
-    #Aqui va el codigo de la cuenta de ahorros
-    '''---------------------------------atributos-----------------------------'''
-
-    saldo=0
-    interes_mensual=0 
-

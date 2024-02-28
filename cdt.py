5dad21bffdd61c175ea0c663715518e43c18bca5
Pecko527 <160885084+Pecko527@users.noreply.github.com>
2024-02-28 12:39:55 -0500

    Delete cdt.py

--- cdt.py
+++
@@ -1,6 +0,0 @@
-class cdt:
-    #Aqui va el codigo del cdt
-    '''---------------------------------atributos-----------------------------'''
-    valorInversion=0
-    interesMensual=0
-    mesApertura=0
